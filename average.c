15ae976dad8f85b9e6ea8b36d25436342bf4381a
rdjirackor <102602995+rdjirackor@users.noreply.github.com>
2022-08-08 11:49:16 +0000

    Add files via upload
    My source code in the c language

---
+++ average.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	float sum=0,count=0,average;
+	
+	for(int i=2;i<6;i++){
+		for(int j=2;j<=i;j++){
+			if(j==i){
+				sum+=i;
+				count++;
+				}
+			else if(i%j==0){
+				break;
+		}
+	}
+}
+	average=sum/count;
+	printf("Average=%.2f",average);
+
+	return 0;
+}
